85af2a0ddeb77d8cebe02f124e707207f2306ba8
SergeiKaplunov <Sergei_kaplunov12@bk.ru>
2019-09-24 09:55:48 +0700

    Добавление коментарий

--- ContactsApp/ContactsAppUI/Form1.Designer.cs
+++ ContactsApp/ContactsAppUI/Form1.Designer.cs
@@ -6,13 +6,14 @@
         /// Обязательная переменная конструктора.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
-
+        
         /// <summary>
         /// Освободить все используемые ресурсы.
         /// </summary>
         /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
-        protected override void Dispose(bool disposing)
+        protected override void Dispose(bool disposing)///коментарии
         {
+
             if (disposing && (components != null))
             {
                 components.Dispose();
